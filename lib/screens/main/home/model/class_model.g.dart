31f18371b5948aeb440a8d0add9e76add15e2470
배명진 <Bae@isang-yongs-MacBook-Pro-2.local>
2020-06-06 14:42:49 +0900

    vscode 에디터로 옮기고 첫 커밋

--- lib/screens/main/home/model/class_model.g.dart
+++ lib/screens/main/home/model/class_model.g.dart
@@ -8,15 +8,21 @@ part of 'class_model.dart';
 
 ClassModel _$ClassModelFromJson(Map<String, dynamic> json) {
   return ClassModel(
-    name: json['name'] as String,
-    start: json['start'] as int,
-    end: json['end'] as int,
+    json['class_name'] as String,
+    json['manager_id'] as String,
+    json['sub_manager_id'] as String,
+    json['description'] as String,
+    json['start_date'] as String,
+    json['end_date'] as String,
   );
 }
 
 Map<String, dynamic> _$ClassModelToJson(ClassModel instance) =>
     <String, dynamic>{
-      'name': instance.name,
-      'start': instance.start,
-      'end': instance.end,
+      'class_name': instance.className,
+      'manager_id': instance.managerId,
+      'sub_manager_id': instance.subManagerId,
+      'description': instance.description,
+      'start_date': instance.startDate,
+      'end_date': instance.endDate,
     };
