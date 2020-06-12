e89badd5d06848f44a3e941589e3cd6a5f1ac27a
배명진 <Bae@isang-yongs-MacBook-Pro-2.local>
2020-06-12 17:28:03 +0900

    출석현황 InfoCard 업데이트, 기존 InfoCard와 AtteCard삭제

--- lib/screens/main/home/home_page.dart
+++ lib/screens/main/home/home_page.dart
@@ -214,56 +214,139 @@ class CheckStatus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        // color: kPrimaryColor.withOpacity(0.03),
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(50),
-          bottomRight: Radius.circular(50),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        InfoCard(
+          title: "출석",
+          iconColor: Color(0xFFFF8C00),
+          count: 60,
+          total: 100,
+          press: () {},
         ),
-      ),
-      child: Wrap(
-        runSpacing: 10,
-        spacing: 20,
-        children: <Widget>[
-          AttendanceCard(
-            title: "출석",
-            iconColor: Color(0xFFFF8C00),
-            effectedNum: 60,
-            totalNum: 100,
-            press: () {},
+        InfoCard(
+          title: "결석",
+          iconColor: Color(0xFFFF2D55),
+          count: 12,
+          total: 100,
+          press: () {},
+        ),
+        InfoCard(
+          title: "지각",
+          iconColor: Colors.blueAccent,
+          count: 18,
+          total: 100,
+          press: () {},
+        ),
+      ],
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  final Color iconColor;
+  final String title;
+  final int count;
+  final int total;
+  final Function press;
+
+  const InfoCard({
+    Key key,
+    this.iconColor,
+    this.title,
+    this.count,
+    this.total,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        onTap: press,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 2,
+                  color: Color(0xFF6DAED9).withOpacity(0.11),
+                ),
+              ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    // wrapped within an expanded widget to allow for small density device
+                    Container(
+                      alignment: Alignment.center,
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        color: iconColor.withOpacity(0.12),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.ac_unit,
+                        color: iconColor,
+                        size: 12,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: kTextColor),
+                        children: [
+                          TextSpan(
+                            text: "$count",
+                            style:
+                                Theme.of(context).textTheme.headline6.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: iconColor,
+                                    ),
+                          ),
+                          TextSpan(
+                            text: "  /$total",
+                            style: TextStyle(
+                              fontSize: 12,
+                              height: 2,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " 명",
+                            style: TextStyle(
+                                fontSize: 13,
+                                height: 2,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          AttendanceCard(
-            title: "결석",
-            iconColor: Color(0xFFFF2D55),
-            effectedNum: 12,
-            totalNum: 100,
-            press: () {},
-          ),
-          AttendanceCard(
-            title: "지각",
-            iconColor: Colors.blueAccent,
-            effectedNum: 18,
-            totalNum: 100,
-            press: () {},
-          ),
-          AttendanceCard(
-            title: "사전협의",
-            iconColor: Color(0xFF5856D6),
-            effectedNum: 10,
-            totalNum: 100,
-            press: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) {
-              //       return DetailsScreen();
-              //     },
-              //   ),
-              // );
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
